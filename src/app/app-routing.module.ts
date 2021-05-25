f1aa04b9b88714cebe950e39f6e8861824ed0f5e
amit <[amirana14325@gmail.com]>
2021-05-25 19:48:52 +0530

    [Amit Rana]Added::adding routing for login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [{ path: 'register', component: RegisterComponent }];
+const routes: Routes = [{ path: 'register', component: RegisterComponent },{ path: 'login', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
