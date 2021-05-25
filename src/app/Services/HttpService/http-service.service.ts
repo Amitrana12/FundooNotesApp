c7692aa903d8447039f654bedf44359d1b900e62
amit <[amirana14325@gmail.com]>
2021-05-25 10:00:04 +0530

    [Amit Rana]Added::created https services and uer register services to communicate with Api

---
+++ src/app/Services/HttpService/http-service.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+//import { environment } from 'src/environments/environment';
+//import { tokenize } from '@angular/compiler/src/ml_parser/lexer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServiceService {
+
+  
+  httpClient: any;
+  constructor(private http:HttpClient) {
+   }
+
+ token: any;
+
+  // post(url: string, data: any, isHeaderRequired : any=false,headers =null){
+  //   this.token=localStorage.getItem('token');
+  //   console.log("token is",this.token);
+  //   let options = {
+  //     headers: new HttpHeaders({
+  //       'Content-Type': 'application/json',
+  //       'Authorization':this.token,
+        
+  //     })
+  //   }
+  //   //return this.http.post(this.baseUrl + url,data,options);
+  //   return this.http.post(this.baseUrl + url,data,isHeaderRequired && headers);
+  // }
+   post(url : string,data :any, isHeaderRequired : any=false,headers =null){
+     console.log(url,data)
+   return this.http.post(url,data,isHeaderRequired && headers)
+   }
+}
---
+++ src/app/Services/userservice/userservice.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpServiceService } from '../HttpService/http-service.service'
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserserviceService {
+  baseUrl=environment.baseUrl
+  constructor(private httpservice:HttpServiceService) { 
+
+  }
+  registerUser(data: any){
+    console.log("Data in userservice",data)
+    return this.httpservice.post(`${this.baseUrl}/api/User/register`,data);
+  }
+}
