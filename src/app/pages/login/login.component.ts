0296a66ccd8709f7ca3f7abf1d58b8c5b2db00b8
amit <[amirana14325@gmail.com]>
2021-05-28 08:09:42 +0530

    [Amit Rana]Added::did littel change now this is working fine now last update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserserviceService } from 'src/app/Services/userservice/userservice.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
   hide=true
   loginForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder ,private route:Router,private userService:UserserviceService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -22,6 +23,20 @@ export class LoginComponent implements OnInit {
   }
   public hasError = (controlName: string, errorName: string) => {
     return this.loginForm.controls[controlName].hasError(errorName);
+  }
+
+  loginUser(loginform:any) {
+    let reqData = {
+      email: loginform.email,
+      password: loginform.password
+
+    }
+    console.log(reqData);
+    this.userService.loginnUser(reqData).subscribe((res:any)=>{
+      console.log(" Login successfully ",res);
+      localStorage.setItem('token',res['id']);
+     // this.route.navigate(['dashboard'])
+    })
   }
 
 }
